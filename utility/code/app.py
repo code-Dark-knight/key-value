da74ac7a2e15c75a6f4e98af2b0f5d698d40c588
yash shah <yashkumarshah22@gmail.com>
2020-11-21 14:14:56 +0530

    Added Flask API's for CRD operations
    - POST /key/insert - insert key values
    - GET  /key/{key} will show the key value
    - GET /alllist will show all the keys
    - DELETE /key/{key} will delete a specific key

--- utility/code/app.py
+++ utility/code/app.py
@@ -1,1 +1,48 @@
-#this is for server application
+from flask import Flask , request
+from flask_restful import Resource, Api
+import configparser
+config = configparser.ConfigParser()
+config.read('../config.ini')
+
+app=Flask(__name__)
+api=Api(app)
+
+
+items=[]
+
+print(config.sections())
+class Datasource(Resource):
+    def get(self, data):
+        print(data)
+        for item in items:
+            if item["key"] == data:
+                return item, 200
+        return {"key" : None}, 404
+
+
+    def post(self, data):
+        data = request.get_json(force=True)
+        print(data.keys())
+        print(data.values())
+        print(data)
+        item = {"key":data["key"],"value":data["value"]}
+        items.append(item)
+        return item , 201
+
+
+    def delete(self, data):
+        for i in range(len(items)):
+            if items[i]['key'] == data:
+                del items[i]
+                break
+        return str(items)
+
+
+class Alllist(Resource):
+    def get(self):
+        return {"items":items}
+
+api.add_resource(Datasource,"/key/<string:data>")
+api.add_resource(Alllist,"/alllist")
+
+app.run(host='0.0.0.0',port=5000)
